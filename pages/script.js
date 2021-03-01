3d40922cd6a58c83f9258f8afacda04a1f4f7c5d
Aleksandr Serebrovskiy <aleksandr-web@yandex.ru>
2021-03-01 15:36:47 +0300

    fixed bugs in request form and added anchor links

--- pages/script.js
+++ pages/script.js
@@ -1,3 +1,20 @@
+//плавная прокрутка якоря
+const anchors = document.querySelectorAll('a[href*="#form"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
+
+//откритие/закрытие мобильного меню
 const btnHeader = document.getElementById('header');
 const btnMenu = document.getElementById('menu');
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -8,7 +25,6 @@ function addClass() {
 function removeClass() {
   mobileMenu.classList.remove('mobile-menu_opened');
 }
-
 
 btnHeader.addEventListener('click', addClass);
 btnMenu.addEventListener('click', removeClass);
